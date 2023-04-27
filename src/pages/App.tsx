c890e160710010093967d59dd46592be15d63eeb
cb0806151 <cb0806151@otc.edu>
2023-04-26 20:27:45 -0500

    uncomment rootstate

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@chakra-ui/react'
+
 import { Header } from '~components/Header'
 
 export const App = (): JSX.Element => {
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -6,6 +6,6 @@ export const store = configureStore({
     reducer: slices,
 })
 
-// type RootState = ReturnType<typeof store.getState>
+type RootState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch
